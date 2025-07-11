22073a8e2bb84a8040be078fe75aa0b88cff028a
hung-senbox <mr.hung@senbox.org>
2025-07-11 15:22:54 +0700

    fix

--- src/internal/domain/response/get_submission_by_condition_response.go
+++ src/internal/domain/response/get_submission_by_condition_response.go
@@ -1,5 +1,19 @@
 package response
 
+import (
+	"time"
+)
+
 type GetSubmissionByConditionResponse struct {
-	Answer string `json:"answer" binding:"required"`
+	SubmissionID uint64 `json:"id"`
+	Key          string `json:"key"`
+	DB           string `json:"db"`
+	QuestionID   string `json:"question_id" binding:"required"`
+	Question     QuestionListData
+	Answer       string    `json:"answer" binding:"required"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type SubmissionData struct {
+	Items []GetSubmissionByConditionResponse `json:"items" binding:"required"`
 }
