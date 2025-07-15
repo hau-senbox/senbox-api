660950d7034f59357dba97a5dd66e14fc59bddf5
HaoVo2007 <hao01638081724@gmail.com>
2025-07-15 10:04:29 +0700

    fix org_id

--- src/internal/domain/response/pdf_response.go
+++ src/internal/domain/response/pdf_response.go
@@ -3,7 +3,7 @@ package response
 type PdfResponse struct {
 	PdfName        string `json:"pdf_name"`
 	Key            string `json:"key"`
-	OrganizationID uint64 `json:"organization_id"`
+	OrganizationID string `json:"organization_id"`
 	Url            string `json:"url"`
 	Extension      string `json:"extension"`
 }
