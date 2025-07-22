40b727d70f74093cc0562fb7641dddc2c50afca6
hung-senbox <mr.hung@senbox.org>
2025-07-22 16:09:47 +0700

    fix api get child, student 4 admin

--- src/internal/domain/response/child_response.go
+++ src/internal/domain/response/child_response.go
@@ -3,8 +3,8 @@ package response
 import "sen-global-api/internal/domain/entity"
 
 type ChildResponse struct {
-	ChildID       string              `json:"child_id"`
-	ChildName     string              `json:"child_name"`
+	ChildID       string              `json:"id"`
+	ChildName     string              `json:"name"`
 	Avatar        string              `json:"avatar"`
 	AvatarURL     string              `json:"avatar_url"`
 	Parent        *entity.SUserEntity `json:"parent,omitempty"`
