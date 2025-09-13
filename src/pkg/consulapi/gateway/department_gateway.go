7eb024bf62f47cbb3c11eefa88655737a8ed0eb8
hung-senbox <mr.hung@senbox.org>
2025-09-13 09:11:17 +0700

    get departments by org

--- src/pkg/consulapi/gateway/department_gateway.go
+++ src/pkg/consulapi/gateway/department_gateway.go
@@ -53,8 +53,46 @@ func (dg *departmentGateway) GetDepartmentsByUser(context *gin.Context) ([]*dto.
 	}
 
 	if gwResp.StatusCode != 200 {
-		return nil, fmt.Errorf("call gateway upload department menu fail: %s", gwResp.Message)
+		return nil, fmt.Errorf("call gateway get departments by user fail: %s", gwResp.Message)
 	}
 
 	return gwResp.Data, nil
+}
+
+func (dg *departmentGateway) GetDepartmentsByOrganization(context *gin.Context, orgID string) ([]*dto.DepartmentGateway, error) {
+	token, exists := context.Get("token")
+	if !exists {
+		return nil, fmt.Errorf("token not found in context")
+	}
+
+	if !exists {
+		return nil, fmt.Errorf("token not found in context")
+	}
+
+	tokenStr, ok := token.(string)
+	if !ok || tokenStr == "" {
+		return nil, fmt.Errorf("invalid token in context")
+	}
+
+	client, err := NewGatewayClient(dg.serviceName, tokenStr, dg.consul, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Call("GET", fmt.Sprintf("/api/v1/gateway/departments/organization/%s", orgID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var gwResp dto.APIGateWayResponse[[]*dto.DepartmentGateway]
+	if err := json.Unmarshal(resp, &gwResp); err != nil {
+		return nil, fmt.Errorf("unmarshal response fail: %w", err)
+	}
+
+	if gwResp.StatusCode != 200 {
+		return nil, fmt.Errorf("call gateway gett departments by organization fail: %s", gwResp.Message)
+	}
+
+	return gwResp.Data, nil
+
 }
