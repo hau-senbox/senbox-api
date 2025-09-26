037f8dee32fbb9154128e0b49ba9ccda1db68bad
hung-senbox <mr.hung@senbox.org>
2025-09-26 14:36:08 +0700

    fix headers

--- src/internal/middleware/headers.go
+++ src/internal/middleware/headers.go
@@ -4,12 +4,18 @@ import "github.com/gin-gonic/gin"
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Disposition")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Connection, Access-Control-Allow-Origin, Accept-Language, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers",
+			"Content-Type, Content-Length, Accept-Encoding, Accept-Language, X-CSRF-Token, Authorization, Cache-Control, X-Requested-With, app_language")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-		c.Writer.Header().Set("Content-Type", "application/json")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
--- src/internal/middleware/security_middleware.go
+++ src/internal/middleware/security_middleware.go
@@ -23,7 +23,8 @@ func (receiver SecuredMiddleware) Secured() gin.HandlerFunc {
 
 		// get app language
 		appLanguage := uint(1) // default
-		if header := context.GetHeader("X-App-Language"); header != "" {
+		if header := context.GetHeader("app_language"); header != "" {
+			log.Info("App language from header:", header)
 			if val, err := strconv.Atoi(header); err == nil {
 				appLanguage = uint(val)
 			}
